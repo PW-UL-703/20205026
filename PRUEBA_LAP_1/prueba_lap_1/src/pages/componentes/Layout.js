1828f7e9586b39624fe12105a73bfbb1081cd6fd
DeanCont <jeanpierotorres@gmail.com>
2023-09-24 13:16:05 -0500

    nuevo

---
+++ PRUEBA_LAP_1/prueba_lap_1/src/pages/componentes/Layout.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+import Link from "next/link"
+
+export default props => (
+    <>
+        <Head>
+            <title>Biblioteca</title>
+        </Head>
+        <header>
+            
+        </header>
+
+        <h1>Biblioteca</h1>
+        
+
+    </>
+)
